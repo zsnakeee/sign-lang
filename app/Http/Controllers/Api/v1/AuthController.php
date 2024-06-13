43789af03a99c0e3ef219186702e5d6cd79dd971
Ziad Talaat <ziadsnake33@gmail.com>
2024-06-13 20:17:26 +0300

    updated api

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -6,42 +6,51 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
-use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', Password::defaults()],
         ]);
+
+        if ($validator->fails())
+            return response()->json(['error' => $validator->errors()->first()], 401);
 
         $user = User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        return $user->createToken($request->device_name)->plainTextToken;
+        return response()->json([
+            'token' => $user->createToken($request->email)->plainTextToken,
+            'user' => $user,
+        ]);
     }
 
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string',
         ]);
 
-        $user = User::where('email', $request->email)->first();
+        if ($validator->fails())
+            return response()->json(['error' => $validator->errors()->first()], 401);
 
-        if (! $user || ! Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'The provided credentials are incorrect.'], 401);
         }
 
-        return $user->createToken($request->device_name)->plainTextToken;
+        return response()->json([
+            'token' => $user->createToken($request->email)->plainTextToken,
+            'user' => $user,
+        ]);
     }
 
     public function logout(Request $request)
