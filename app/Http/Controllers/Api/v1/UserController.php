9b0839b313f61f2c1296bf263c45d25bf11b9582
Ziad Talaat <ziadsnake33@gmail.com>
2024-06-13 20:38:19 +0300

    updated api

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -4,24 +4,29 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function update_profile(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'first_name' => 'required|string',
             'last_name' => 'required|string',
             'phone' => 'required|string',
         ]);
 
-        $user = $request->user();
-        $user->update($request->all());
-        return response()->json($user);
+        if ($validator->fails())
+            return response()->json(['error' => $validator->errors()->first()], 401);
+
+        try {
+            $user = $request->user();
+            $user->update($request->all());
+            return response()->json($user);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 401);
+        }
     }
-
-
-
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\API\v1\UserController ;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,7 @@ Route::get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1'], function () {
     Route::group(['middleware' => 'auth:sanctum'], function () {
-        Route::post('user/update/profile', [AuthController::class, 'update_profile']);
+        Route::post('user/update/profile', [UserController::class, 'update_profile']);
     });
 
     Route::group(['prefix' => 'auth'], function () {
